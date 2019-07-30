e6741361060ca59c2371e04a7992c818953625c7
Ihor <ShadoofJumper@gmail.com>
2019-07-30 22:01:43 +0300

    Create door in progress

---
+++ Assets/Scripts/DoorOpenDevice.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpenDevice : MonoBehaviour
+{
+    [SerializeField] private Vector3 dpos;
+
+
+    private void Operate()
+    {
+        bool _open = false;
+        if (_open)
+        {
+            Vector3 pos = transform.position - dpos;
+            transform.position = pos;
+        }
+        else
+        {
+            Vector3 pos = transform.position + dpos;
+            transform.position = pos;
+        }
+
+        _open = !_open;
+    }
+}
